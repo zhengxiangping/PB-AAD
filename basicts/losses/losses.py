6897bbac263a5f989385da6e1ff2ac6a151fb91e
zhengxiangping <1024548147@qq.com>
2026-02-12 10:22:39 +0800

    Update losses.py

--- basicts/losses/losses.py
+++ basicts/losses/losses.py
@@ -1,11 +1,4 @@
-'''
-Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
-Date: 2024-07-14 22:27:47
-LastEditors: wubo2180 15827403235@163.com
-LastEditTime: 2024-07-31 17:11:31
-FilePath: \python_workspace\GPT-GNN-ATEST\basic\losses\losses.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
+
 import torch
 import torch.nn.functional as F
 
